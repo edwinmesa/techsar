65c553ad2480b2920c88aa495a8b1f322b6e99bd
DESKTOP-1S21JGE\devsar <edwinsar.mes@gmail.com>
2019-02-14 13:22:16 -0500

    Create table WareHouse

--- database/migrations/2019_02_10_162831_create_warehouse_table.php
+++ database/migrations/2019_02_10_162831_create_warehouse_table.php
@@ -18,14 +18,7 @@ class CreateWarehouseTable extends Migration
             $table->timestamps();
             $table->integer('f154_mc_cod_warehouse')->unique();
             $table->string('f154_mc_des_warehouse');
-            $table->unsignedInteger('f154_mc_id_item');
-            $table->foreign('f154_mc_id_item')->references('f153_mc_id_item')->on('t153_mc_items');
-            $table->string('f154_mc_model_item');
-            $table->integer('f154_mc_ean_item');
-            $table->string('f154_mc_provider_item');
-            $table->integer('f154_mc_cant_init')->default(0);
-            $table->integer('f154_mc_cant_disp')->default(0);
-            $table->integer('f154_mc_cant_falt')->default(0);
+
         });
     }
 
