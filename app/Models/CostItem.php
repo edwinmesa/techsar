a85a06d58b4a6e956f6158f124c59d05a409dbd6
DESKTOP-1S21JGE\devsar <edwinsar.mes@gmail.com>
2019-02-14 17:35:27 -0500

    Model and Seeder of Costtem

---
+++ app/Models/CostItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CostItem extends Model
+{
+    protected $table        = 't200_mvt_cost_items';
+    protected $primaryKey   = 'f200_mvt_id_cost_item';
+}
---
+++ database/seeds/CostItemSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CostItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
