4e68231c5acf6e0cf797b4d802aa8691a0467132
Edwin Mesa <edwinsar.mes@gmail.com>
2019-02-13 22:34:36 -0500

    Modificacion de Factory Item -- Completo

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -20,26 +20,33 @@ $factory->define(App\Models\Item::class, function (Faker $faker) {
      * para los grupos informatica y electronica.
      */
     $brandId = DB::table('t150_mc_brands')
-        ->whereBetween('f150_mc_id_brand', array(1,20))->get();
-    $value = $brandId->random()->f150_mc_id_brand;
-
-//    $values =Brand::find([$value])->f150_mc_brand_des->get();
-
-    $values = DB::table('t150_mc_brands')->where('f150_mc_id_brand', $value)->value('f150_mc_brand_des');
-
+        ->whereBetween('f150_mc_id_brand', array(1,6))->get();
+    $valueBrandId = $brandId->random()->f150_mc_id_brand;
+    $valueBrandDes = DB::table('t150_mc_brands')
+        ->where('f150_mc_id_brand', $valueBrandId)
+        ->value('f150_mc_brand_des');
+    $valueBrandAbv = DB::table('t150_mc_brands')
+        ->where('f150_mc_id_brand', $valueBrandId)
+        ->value('f150_mc_brand_abv');
+    $valueProviderId = DB::table('t150_mc_brands')
+        ->where('f150_mc_id_brand', $valueBrandId)
+        ->value('f150_mm_id_provider');
+    $valueProviderDes = DB::table('t112_mm_providers')
+        ->where('f112_mm_id_providers', $valueProviderId)
+        ->value('f112_mm_provider_des');
 
     static $itemCod = 100;
     return [
         'f153_mc_item_cod'          => $itemCod++,
-        'f153_mc_item_des'          => $faker->monitor.' '.$values,
+        'f153_mc_item_des'          => $faker->monitor.' '.$valueBrandDes,
         'f153_mc_item_active'       => 1,
-        'f153_mc_item_model'        => $faker->monitor.$faker->randomNumber(2),
+        'f153_mc_item_model'        => $valueBrandAbv.' '.$faker->models.$faker->randomNumber(2),
         'f153_mc_item_ean'          => '00'.$faker->ean8,
         'f153_mc_item_date_fab'     => $faker->dateTimeBetween('-16 years', $endDate = '-12 years', $timezone = null),
         'f153_mc_item_date_ven'     => $faker->dateTimeBetween('-7 years', $endDate = '-2 years', $timezone = null),
-        'f153_mc_item_provider'     => $faker->company,
+        'f153_mc_item_provider'     => $valueProviderDes,
         'f153_mc_id_category'       => Category::all()->random()->f152_mc_id_category,
-        'f153_mc_id_brand'          =>$value,
+        'f153_mc_id_brand'          =>$valueBrandId,
 //        'f153_mc_item_des'          => function (array $post) {
 //                                        return App\Models\Brand::find($post['f153_mc_id_brand'])->f150_mc_brand_des;
 //                                        }
