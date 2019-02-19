9cd9f75af0b90c85cbd7d6544222420f7a376885
Edwin Mesa <edwinsar.mes@gmail.com>
2019-02-18 21:11:17 -0500

    changes

--- database/migrations/2019_02_14_163610_create_mov_inventories_table.php
+++ database/migrations/2019_02_14_163610_create_mov_inventories_table.php
@@ -13,9 +13,25 @@ class CreateMovInventoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('mov_inventories', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('t250_mvt_inventories', function (Blueprint $table) {
+            $table->increments('f200_mvt_id_inventories');
             $table->timestamps();
+            $table->integer('f250_mvt_item_cod')->unique();
+            $table->string('f250_mvt_item_des');
+            $table->string('f250_mvt_item_model')->nullable();;
+            $table->string('f250_mvt_item_ean');
+            $table->string('f250_mvt_brand');
+            $table->string('f250_mvt_provider');
+            $table->string('f250_mvt_category');
+            $table->string('f250_mvt_id_category');
+            $table->integer('f250_mvt_cant_initial');
+            $table->integer('f250_mvt_cant_available');
+            $table->integer('f250_mvt_cant_lack');
+            $table->double('f250_mvt_cost_initial');
+            $table->double('f250_mvt_cost_manufacture');
+            $table->double('f250_mvt_cost_sales');
+            $table->double('f250_mvt_cost_delivery');
+            $table->double('f250_mvt_cost_taxes');
         });
     }
 
@@ -26,6 +42,6 @@ class CreateMovInventoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('mov_inventories');
+        Schema::dropIfExists('t250_mvt_inventories');
     }
 }
--- database/seeds/WarehouseSeeder.php
+++ database/seeds/WarehouseSeeder.php
@@ -15,9 +15,16 @@ class WarehouseSeeder extends Seeder
     {
         Warehouse::create([
             'f154_mc_cod_warehouse'     => 6501,
-            'f154_mc_des_warehouse'     => 'Bogota',
+            'f154_mc_des_warehouse'     => 'Bodega General Bogota',
             'f154_mc_id_op'             =>  $groupId = OperationCenter::where('f102_mm_op_cod', 6494)
                                             ->value('f102_mm_id_op'),
+        ]);
+
+        Warehouse::create([
+            'f154_mc_cod_warehouse'     => 6502,
+            'f154_mc_des_warehouse'     => 'Bodega General Medellin',
+            'f154_mc_id_op'             =>  $groupId = OperationCenter::where('f102_mm_op_cod', 6494)
+                ->value('f102_mm_id_op'),
         ]);
     }
 }
